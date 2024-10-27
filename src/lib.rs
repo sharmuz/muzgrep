d3affe5a7fa84f3fd2b6ec4f94dd7542b967da9c
sharmuz <muzher@gmail.com>
2024-10-27 16:44:07 +0000

    Add case insensitive search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
+use std::env;
 use std::fs;
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -15,14 +17,22 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for result in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for result in results {
         println!("{result}");
     }
     
@@ -34,6 +44,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -57,6 +80,38 @@ mod tests {
 
         // CALL
         let result = search(query, contents);
+        
+        // ASSERT
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn case_sensitive() {
+        // SETUP
+        let query = "pen";
+        let contents = "\
+            My pen!\
+            \nPenny gave it to me.";
+        let expected = vec!["My pen!"];
+
+        // CALL
+        let result = search(query, contents);
+        
+        // ASSERT
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        // SETUP
+        let query = "pen";
+        let contents = "\
+            My pen!\
+            \nPenny gave it to me.";
+        let expected = vec!["My pen!", "Penny gave it to me."];
+
+        // CALL
+        let result = search_case_insensitive(query, contents);
         
         // ASSERT
         assert_eq!(result, expected);
