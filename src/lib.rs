a112e8aa09f4afab52610c7cead86a0e3f036ed5
sharmuz <muzher@gmail.com>
2024-11-17 15:49:33 +0000

    Refactor config and search to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("missing required arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();  // Gives name of program
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No query string specified"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No file path specified")
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
@@ -40,28 +43,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|l| l.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    let query_lower = query.to_lowercase();
+    
+    contents
+        .lines()
+        .filter(|l| l.to_lowercase().contains(&query_lower))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ use std::process;
 use muzgrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // dbg!(args);
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Error attempting to parse user arguments: {err}");
         process::exit(1);
     });
